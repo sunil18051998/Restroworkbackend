5b864fa0a98adcf71c3e41d0bc004b678975af0d
Sunil Maurya <mauryasunilkumar98@gmail.com>
2025-09-25 09:07:48 +0530

    products update

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -8,13 +8,13 @@ import Footer from "../../../components/Footer";
 import Productssection from "./products";
 
 async function getProducts(locale: string = "en") {
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/products?locale=${locale}&limit=20`,
-  //   { cache: "no-store" }
-  // );
-  // const data = await res.json();
-  // return data.docs;
-  return [];
+  const res = await fetch(
+    `https://restroworkbackend.vercel.app/api/products?locale=${locale}&limit=20`,
+    { cache: "no-store" }
+  );
+  const data = await res.json();
+  return data.docs;
+  //return [];
 }
 
 export default async function ProductsPage() {
